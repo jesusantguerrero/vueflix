1475babbf41ace24d83608bcf3bcb97a22f28a25
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-12 19:54:59 -0400

    feat: add lazy image to program item

--- src/components/ProgramItem.vue
+++ src/components/ProgramItem.vue
@@ -1,5 +1,8 @@
 <script setup>
-defineProps({
+import { computed, ref } from "vue";
+import VLazyImage from "v-lazy-image";
+
+const props = defineProps({
   program: {
     type: Object,
   },
@@ -7,38 +10,45 @@ defineProps({
     type: Boolean,
   },
 });
+
+const hasImageErrors = ref(false);
+const handleLoadError = () => {
+  hasImageErrors.value = true;
+};
+
+const imageUrl = computed(() => {
+  return hasImageErrors.value ? null : props.program.images["Poster Art"].url;
+});
 </script>
 
 <template>
-  <article
-    class="rounded-md overflow-hidden border shadow-md group"
-    :key="program.id"
-  >
+  <article class="group rounded-md overflow-hidden border shadow-md group">
     <div
-      class="w-full h-44 bg-gray-300 overflow-hidden relative cursor-pointer"
+      class="w-full text-gray-200 font-bold h-44 flex items-center justify-center bg-gradient-to-br from-red-400 to-red-800 overflow-hidden relative cursor-pointer"
     >
-      <img
-        :src="program.images['Poster Art'].url"
-        class="w-full h-full object-cover object-center"
+      <v-lazy-image
+        v-if="!hasImageErrors"
+        :src="imageUrl"
+        @error="handleLoadError"
+        @onload="handledLoaded"
+        class="w-full h-full group-hover:scale-110 transform ease-in-out transition-transform object-cover object-center"
+        :alt="program.title"
       />
-      <div
-        v-if="imageOnly"
-        class="transition px-4 py-2 absolute animate-ease-[cubic-bezier(0.25,0.1,0.25,1)] animate-bubble -top-52 bg-gray-300 h-full w-full z-10 group-hover:top-0 overflow-ellipsis"
-      >
-        <h4 class="font-bold text-gray-600">
-          {{ program.title }}
-        </h4>
-        <p>{{ program.description }}</p>
-      </div>
+      <span v-else class="px-4"> {{ program.title }} </span>
     </div>
     <section class="px-4 py-2" v-if="!imageOnly">
       <h4 class="font-bold text-gray-600">
         {{ program.title }}
       </h4>
-      <p>{{ program.description }}</p>
-      <footer class="mt-2 flex justify-between">
-        Released: {{ program.releaseYear }}
-        <span class="capitalize">{{ program.programType }}</span>
+      <p class="text-gray-500 italic mt-4">{{ program.description }}</p>
+      <footer class="mt-2 flex justify-between text-gray-400">
+        <p>
+          <span class="italic"> Released: </span>
+          <span class="text-red-400 font-bold">
+            {{ program.releaseYear }}
+          </span>
+        </p>
+        <span class="capitalize text-gray-600">{{ program.programType }}</span>
       </footer>
     </section>
   </article>
