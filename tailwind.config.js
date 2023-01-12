1fb17c4f408f7c883415e3ee34778f112e237ea8
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-11 20:13:48 -0400

    chore: setup tailwind and atmosphere-ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,7 @@ module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
-    "./node_modules/atmosphere-ui/**/*.{vue,js}",
+    "node_modules/atmosphere-ui/**/*.{vue,js}",
   ],
   theme: {
     extend: {},
