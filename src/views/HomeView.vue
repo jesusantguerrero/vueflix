ccd6424049bcb9c597066368d99f762886174f85
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-12 18:34:19 -0400

    feat: Add layout to home view and add comments

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,13 +5,20 @@ import ProgramItem from "@/components/ProgramItem.vue";
 import AppSearch from "@/components/AppSearch.vue";
 import { useFilters } from "@/composables/useFilters";
 import AppSearchFilterLabels from "../components/AppSearchFilterLabels.vue";
+import AppLayout from "./Partials/AppLayout.vue";
 
 const store = useStore();
 
+// Initial load
 const series = computed(() => {
   return store.getters["movies/series"].slice(1, 5);
 });
 
+onMounted(() => {
+  store.dispatch("movies/fetchMovies");
+});
+
+//  Search behavior
 const displaySearch = computed(() => {
   return store.getters["movies/shouldDisplayResults"];
 });
@@ -24,50 +31,56 @@ const results = computed(() => {
   return store.getters["movies/results"];
 });
 
-const { filters, sorts, reset } = useFilters((finalUrl) => {
+const { filters, sorts, searchText, reset } = useFilters((finalUrl) => {
   store.dispatch("movies/executeSearch", finalUrl);
-});
-
-onMounted(() => {
-  store.dispatch("movies/fetchMovies");
 });
 </script>
 
 <template>
-  <main class="max-w-7xl mx-auto space-y-4 pb-50 py-4">
-    <AppSearch
-      v-model="searchText"
-      v-model:filters="filters"
-      v-model:sorts="sorts"
-      :has-filters="displaySearch"
-      @clear="reset()"
-    />
-    <AppSearchFilterLabels v-model:filters="filters" v-model:sorts="sorts" />
+  <AppLayout>
+    <!-- To set the search in the header let the controls here -->
+    <template #header>
+      <AppSearch
+        v-model="searchText"
+        v-model:filters="filters"
+        v-model:sorts="sorts"
+        class="w-full"
+        :has-filters="displaySearch"
+        @clear="reset()"
+      />
+    </template>
 
-    <template v-if="!displaySearch">
-      <section class="max-w-7xl mx-auto mt-12">
-        <h2 class="font-bold text-gray-500 text-lg">Series</h2>
+    <main class="max-w-7xl mx-auto space-y-4 pb-50 py-4">
+      <!-- Let's show the current filters -->
+      <AppSearchFilterLabels v-model:filters="filters" v-model:sorts="sorts" />
+
+      <!-- Lets get some fun showing spotlight -->
+      <template v-if="!displaySearch">
+        <section class="max-w-7xl mx-auto mt-12">
+          <h2 class="font-bold text-gray-500 text-lg">Series</h2>
+          <section class="grid md:grid-cols-4 gap-2 mt-4">
+            <ProgramItem
+              v-for="(program, index) in series"
+              :key="`${program.title}-${index}`"
+              :program="program"
+              class="h-32"
+              image-only
+            />
+          </section>
+        </section>
+      </template>
+
+      <!-- The rest of the list an search results -->
+      <section class="mt-4">
+        <h2 class="font-bold text-gray-500 text-lg">{{ sectionTitle }}</h2>
         <section class="grid md:grid-cols-4 gap-2 mt-4">
           <ProgramItem
-            v-for="(program, index) in series"
-            :key="`${program.title}-${index}`"
+            v-for="program in results"
+            :key="program.id"
             :program="program"
-            class="h-32"
-            image-only
           />
         </section>
       </section>
-    </template>
-
-    <section class="mt-4">
-      <h2 class="font-bold text-gray-500 text-lg">{{ sectionTitle }}</h2>
-      <section class="grid md:grid-cols-4 gap-2 mt-4">
-        <ProgramItem
-          v-for="program in results"
-          :key="program.id"
-          :program="program"
-        />
-      </section>
-    </section>
-  </main>
+    </main>
+  </AppLayout>
 </template>
