35680c331ad66094665779af257d8110aa57fa5f
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-12 17:18:28 -0400

    refactor: improve useFilter composable with tests

---
+++ src/components/__tests__/filters.spec.js
@@ -0,0 +1,106 @@
+import { describe, it, expect } from "vitest";
+import {
+  parseFilterParams,
+  parseParams,
+  parseSortParams,
+} from "../../composables/useFilters";
+
+const paramsConfig = {
+  sorts: {
+    title: {
+      value: true,
+      direction: "desc",
+    },
+    releaseYear: {
+      value: true,
+      direction: "asc",
+    },
+  },
+  filters: {
+    releaseYear: {
+      value: "2016",
+    },
+    programType: {
+      value: "series",
+    },
+  },
+};
+
+const emptyParamsConfig = {
+  sorts: {
+    title: {
+      value: false,
+      direction: "desc",
+    },
+    releaseYear: {
+      value: false,
+      direction: "asc",
+    },
+  },
+  filters: {
+    releaseYear: {
+      value: "",
+    },
+    programType: {
+      value: "",
+    },
+  },
+};
+describe("Filters Transforms", async () => {
+  it("transform one sort properly", async () => {
+    const params = parseSortParams({
+      title: {
+        value: true,
+        direction: "asc",
+      },
+      releaseYear: {
+        value: false,
+        direction: "",
+      },
+    });
+    expect(params).toBe("_sort=title&_order=asc");
+  });
+
+  it("transform multiple sorts properly", async () => {
+    const params = parseSortParams({
+      title: {
+        value: true,
+        direction: "desc",
+      },
+      releaseYear: {
+        value: true,
+        direction: "asc",
+      },
+    });
+    expect(params).toBe("_sort=title,releaseYear&_order=desc,asc");
+  });
+
+  it("transforms one filter properly", async () => {
+    const params = parseFilterParams({
+      releaseYear: {
+        value: "2016",
+      },
+    });
+    expect(params).toBe("releaseYear=2016");
+  });
+
+  it("transforms multiple filter properly", async () => {
+    const params = parseFilterParams(paramsConfig.filters);
+
+    expect(params).toBe("releaseYear=2016&programType=series");
+  });
+
+  it("it should combine filter and sort params", async () => {
+    const params = parseParams(paramsConfig);
+
+    expect(params).toBe(
+      "releaseYear=2016&programType=series&_sort=title,releaseYear&_order=desc,asc"
+    );
+  });
+
+  it("it should be empty", async () => {
+    const params = parseParams(emptyParamsConfig);
+
+    expect(params).toBeFalsy();
+  });
+});
--- src/composables/useFilters.js
+++ src/composables/useFilters.js
@@ -1,41 +1,84 @@
-import { reactive, watch } from "vue";
+import { reactive, toRefs, watch } from "vue";
+
+export const parseFilterParams = (filterConfig) => {
+  return Object.entries(filterConfig)
+    .reduce((filters, [name, filter]) => {
+      if (filter.value) {
+        filters.push(`${name}=${filter.value}`);
+      }
+      return filters;
+    }, [])
+    .join("&");
+};
+
+export const parseSortParams = (sortConfig) => {
+  const params = Object.entries(sortConfig).reduce(
+    (sortValues, [name, sort]) => {
+      if (sort.value) {
+        sortValues._sort.push(name);
+        sortValues._order.push(sort.direction);
+      }
+      return sortValues;
+    },
+    {
+      _sort: [],
+      _order: [],
+    }
+  );
+  return (
+    Object.entries(params)
+      // eslint-disable-next-line no-unused-vars
+      .filter(([_name, sorts]) => sorts.length)
+      .map(([name, values]) => `${name}=${values.join(",")}`)
+      .join("&")
+  );
+};
+
+export const parseParams = (paramsConfig) => {
+  let params = [
+    parseFilterParams(paramsConfig.filters),
+    parseSortParams(paramsConfig.sorts),
+  ];
+
+  return params.filter((value) => value?.trim()).join("&");
+};
+
+const DEFAULT_CONFIG = {
+  searchText: "",
+  filters: {
+    releaseYear: {
+      label: "Year",
+      value: "",
+    },
+    programType: {
+      label: "Type",
+      value: "",
+      options: ["movie", "series"],
+    },
+  },
+  sorts: {
+    title: {
+      label: "Name",
+      value: "",
+      direction: "",
+    },
+    releaseYear: {
+      label: "Year",
+      value: "",
+      direction: "",
+    },
+  },
+};
 
 export const useFilters = (onUrlChange) => {
-  const state = reactive({
-    searchText: "",
-    filters: {
-      year: "",
-      programType: "",
-      movie: "",
-      series: "",
-    },
-    sorts: "",
-  });
-
-  const filterParams = (externalFilters) => {
-    return Object.entries(externalFilters)
-      .reduce((filters, [name, value]) => {
-        if (value) {
-          filters.push(`${name}=${value}`);
-        }
-        return filters;
-      }, [])
-      .join("&");
-  };
-
-  const getSortsParams = (sorts) => {
-    return sorts && `_sort=${sorts}`;
-  };
+  const state = reactive({ ...DEFAULT_CONFIG });
 
   watch(
     () => state,
-    (state) => {
-      let params = [filterParams(state.filters), getSortsParams(state.sorts)];
-
-      const urlParams = params
-        .filter((value) => value && value?.trim())
-        .join("&");
+    (paramsConfig) => {
+      const urlParams = parseParams(paramsConfig);
       const finalUrl = `${window.location.pathname}?${urlParams}`;
+
       if (finalUrl != window.location.toString()) {
         onUrlChange && onUrlChange(urlParams);
       }
@@ -43,5 +86,14 @@ export const useFilters = (onUrlChange) => {
     { deep: true }
   );
 
-  return state;
+  const reset = () => {
+    state.searchText = DEFAULT_CONFIG.searchText;
+    state.filters = DEFAULT_CONFIG.filters;
+    state.sorts = DEFAULT_CONFIG.sorts;
+  };
+
+  return {
+    ...toRefs(state),
+    reset,
+  };
 };
