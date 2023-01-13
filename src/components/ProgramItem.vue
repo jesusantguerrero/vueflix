ac59808df309c033b05be45d2db3e30ad73f3ea1
Jesus Guerrero <jesusant.guerrero@gmail.com>
2023-01-13 00:49:23 -0400

    style: improve program item styles

--- src/components/ProgramItem.vue
+++ src/components/ProgramItem.vue
@@ -19,36 +19,52 @@ const handleLoadError = () => {
 const imageUrl = computed(() => {
   return hasImageErrors.value ? null : props.program.images["Poster Art"].url;
 });
+
+const isHovered = ref(false);
+const title = computed(() => {
+  return !isHovered.value
+    ? props.program.title.slice(0, 32) + "..."
+    : props.program.title;
+});
 </script>
 
 <template>
-  <article class="group rounded-md overflow-hidden border shadow-md group">
+  <article
+    @mouseover="isHovered = true"
+    @mouseleave="isHovered = false"
+    class="group rounded-md overflow-hidden border shadow-md group relative"
+  >
     <div
-      class="w-full text-gray-200 font-bold h-44 flex items-center justify-center bg-gradient-to-br from-red-400 to-red-800 overflow-hidden relative cursor-pointer"
+      class="w-full text-gray-200 font-bold h-72 flex items-center justify-center bg-gradient-to-br from-red-400 to-red-800 overflow-hidden relative cursor-pointer"
     >
       <v-lazy-image
         v-if="!hasImageErrors"
         :src="imageUrl"
         @error="handleLoadError"
         @onload="handledLoaded"
-        class="w-full h-full group-hover:scale-110 transform ease-in-out transition-transform object-cover object-center"
+        class="w-full h-full group-hover:scale-110 transform ease-in-out transition-transform object-cover object-top"
         :alt="program.title"
       />
       <span v-else class="px-4"> {{ program.title }} </span>
     </div>
-    <section class="px-4 py-2" v-if="!imageOnly">
-      <h4 class="font-bold text-gray-600">
-        {{ program.title }}
+    <section
+      class="w-full ease-linear group-hover:pb-20 cursor-pointer group-hover:top-0 transition px-4 py-4 bg-gradient-to-b from-transparent via-gray-800 to-gray-900 absolute text-white top-52"
+      v-if="!imageOnly"
+    >
+      <h4 class="font-bold w-full flex overflow-ellipsis">
+        {{ title }}
       </h4>
-      <p class="text-gray-500 italic mt-4">{{ program.description }}</p>
-      <footer class="mt-2 flex justify-between text-gray-400">
+      <p class="hidden group-hover:block text-gray-200 italic mt-4">
+        {{ program.description }}
+      </p>
+      <footer class="flex group-hover:mt-8 justify-between text-gray-200">
         <p>
           <span class="italic"> Released: </span>
           <span class="text-red-400 font-bold">
             {{ program.releaseYear }}
           </span>
         </p>
-        <span class="capitalize text-gray-600">{{ program.programType }}</span>
+        <span class="capitalize text-gray-200">{{ program.programType }}</span>
       </footer>
     </section>
   </article>
--- src/views/Partials/HomeSpotlight.vue
+++ src/views/Partials/HomeSpotlight.vue
@@ -11,18 +11,20 @@ import { AtButton } from "atmosphere-ui";
     }"
   >
     <div
-      class="w-full h-full absolute bg-gradient-to-b from-transparent md:via-white/10 via-white/40 to-white"
+      class="w-full h-full absolute bg-gradient-to-b from-transparent  via-white/40 to-white"
     ></div>
     <footer
-      class="px-4 py-2 absolute bottom-8 flex flex-col md:flex-row justify-between w-full"
+      class="px-4 py-2 absolute bottom-8 flex flex-col md:flex-row justify-between w-full md:order-1"
     >
       <AtButton
-        class="text-white mt-10 md:mt-0 order-2 min-w-fit h-12 bg-red-600 rounded-md"
+        class="text-white mt-10 font-bold hover:scale-105 transform md:mt-0 order-2 min-w-fit h-12 bg-red-600 rounded-md"
       >
         Watch now
       </AtButton>
 
-      <h4 class="text-4xl md:text-gray-600 text-red-500 font-bold order-1">
+      <h4
+        class="text-4xl md:text-gray-600 text-red-500 font-bold order-1 md:order-2"
+      >
         Sherlock: The Abominable Bride
       </h4>
     </footer>
